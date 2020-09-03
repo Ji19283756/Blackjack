a800eddb761fcb6deeee635d952e5d8db3ded79b
Ji19283756 <Ji19283756@gmail.com>
2020-09-03 14:19:59 -0400

    Added the dealer ai and a functino that evaluates who wins

--- main.py
+++ main.py
@@ -98,7 +98,7 @@ class Player:
         total_value = 0
         # print("set_card_value")
         for card in self.cards:
-            #    print(card)
+            # print("card: "+card)
             total_value += get_card_value(card)
         #    print(get_card_value(card))
         self.card_value = total_value
@@ -146,19 +146,36 @@ def play_black_jack():
                             player.cards += deck.permanant_removal_pick_a_card(1)
                             print("You got " + str(player.cards[-1]))
                             player.set_card_value()
+                            print("This gives you a total card value of " + str(player.card_value))
                         elif player_decision == 4:
                             print("You decide not to draw another card")
                         elif player_decision == 5:
                             print("You decide to end the game")
                             game_will_be_done = True
                         break
-                    #if player_decision == 3 or player_decision == 4 or player_decision == 5:
+                    # if player_decision == 3 or player_decision == 4 or player_decision == 5:
                     if 3 <= player_decision <= 5:
                         break
 
             except ValueError:
                 print("Please print a valid number")
         return game_will_be_done
+
+    def dealer_ai(real_player, self_dealer):
+        dealer_amount = self_dealer.card_value
+        dealer_game_will_be_done = False
+        if dealer_amount < 19 or (dealer_amount >= 19 and (20 < real_player.card_value < 22)):
+            print("The dealer draws another card")
+            self_dealer.cards += deck.permanant_removal_pick_a_card(1)
+            self_dealer.set_card_value()
+            print("The dealer got " + str(player.cards[-1]) + "\nThis gives him a total value of "
+                  + str(self_dealer.card_value))
+
+        elif dealer_amount >= 19:
+            print("The dealer decides to end the game")
+            dealer_game_will_be_done = True
+
+        return dealer_game_will_be_done
 
     deck = Deck()
     deck.shuffle()
@@ -172,30 +189,52 @@ def play_black_jack():
     drawn_card = deck.permanant_removal_pick_a_card(1)
     print("The dealer got " + str(drawn_card[0]) + "\n")
     dealer = Player(drawn_card)
-    game_will_be_done = False
+
+    player_game_will_be_done = False
+    dealer_game_will_be_done = False
     while True:
-        if game_will_be_done:
+        if player_game_will_be_done:
             break
 
         if player.is_not_bust():
-            game_will_be_done = ask_player_for_choice(dealer)
+            player_game_will_be_done = ask_player_for_choice(dealer)
         else:
             print("You went bust, your cards were:")
             for card in player.cards:
                 print(card)
             print("Giving you a total of " + str(player.card_value) + " which means you lose")
             break
-        if player.won():
-            print("You win")
+        if player.won() or not player.is_not_bust():
             break
 
-        # if dealer.is_not_bust():
-        #    pass
-        # else:
-        #    break
+        if dealer_game_will_be_done:
+            break
 
+        if dealer.is_not_bust():
+            dealer_game_will_be_done = dealer_ai(player, dealer)
+        else:
+            break
+
+        if dealer.won() or not dealer.is_not_bust():
+            break
+
+    def evaluate_winner(player1, dealer1):
+        if 21 >= player1.card_value > dealer1.card_value:
+            print("You win")
+        elif 21 >= dealer1.card_value > player1.card_value:
+            print("The dealer won")
+        elif player1.card_value>21:
+            print("You go bust\nYou Lose and the Dealer wins")
+        elif dealer1.card_value>21:
+            print("The dealer goes bust\nYou Win")
+        else:
+            print("Its a tie")
+    evaluate_winner(player, dealer)
 
 play_black_jack()
+
+
+
 
 # for pile in piles:
 #    print(pile)
