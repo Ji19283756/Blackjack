a5a9ba96ab7af4301b92925d12ce783a53084e07
Ji19283756 <Ji19283756@gmail.com>
2020-11-15 16:06:11 -0500

    Added the dealer ai and a functino that evaluates who wins

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import random
-
+from random import choice, shuffle
 
 class Deck:
     def __init__(self, joker=False):
@@ -21,15 +21,8 @@ class Deck:
         if joker:
             self.cards += ["Joker", "Joker"]
 
-    def shuffle(self, new_order=[]):
-
-        for x in range(len(self.cards)):
-            rng = random.randint(0, len(self.cards) - 1)
-            new_order += [self.cards[rng]]
-            self.cards.remove(self.cards[rng])
-
-        self.cards = new_order
-
+    def shuffle(self):
+        shuffle(self.cards)
         return self.cards
 
     def pick_a_card(self, amount, replace=True, taken_cards=[]):
@@ -129,36 +122,35 @@ def play_black_jack():
                     "\n-(3)draw another card\n-(4)don't draw another card\n-(5)End game, opponent has"
                     " options to draw one more time\n"))
                 if 1 <= player_decision <= 5:
-                    while True:
-                        print()
-                        if player_decision == 1:
-                            print("Your cards are:")
-                            for card in player.cards:
-                                print(card)
-                            print("This means that your total is " + str(player.card_value) + "\n")
-                        elif player_decision == 2:
-                            print("Your opponent's cards are:")
-                            for card in enemy.cards:
-                                print(card)
-                            print("Your opponent's total is " + str(enemy.card_value) + "\n")
-                        elif player_decision == 3:
-                            print("The dealer draws another card")
-                            player.cards += deck.permanant_removal_pick_a_card(1)
-                            print("You got " + str(player.cards[-1]))
-                            player.set_card_value()
-                            print("This gives you a total card value of " + str(player.card_value))
-                        elif player_decision == 4:
-                            print("You decide not to draw another card")
-                        elif player_decision == 5:
-                            print("You decide to end the game")
-                            game_will_be_done = True
-                        break
-                    # if player_decision == 3 or player_decision == 4 or player_decision == 5:
+                    print()
+                    if player_decision == 1:
+                        print("Your cards are:")
+                        for card in player.cards:
+                            print(card)
+                        print("This means that your total is " + str(player.card_value) + "\n")
+                    elif player_decision == 2:
+                        print("Your opponent's cards are:")
+                        for card in enemy.cards:
+                            print(card)
+                        print("Your opponent's total is " + str(enemy.card_value) + "\n")
+                    elif player_decision == 3:
+                        print("The dealer draws another card")
+                        player.cards += deck.permanant_removal_pick_a_card(1)
+                        print("You got " + str(player.cards[-1]))
+                        player.set_card_value()
+                        print("This gives you a total card value of " + str(player.card_value))
+                    elif player_decision == 4:
+                        print("You decide not to draw another card")
+                    elif player_decision == 5:
+                        print("You decide to end the game")
+                        game_will_be_done = True
+
                     if 3 <= player_decision <= 5:
                         break
 
             except ValueError:
                 print("Please print a valid number")
+
         return game_will_be_done
 
     def dealer_ai(real_player, self_dealer):
@@ -212,6 +204,7 @@ def play_black_jack():
 
         if dealer.is_not_bust():
             dealer_game_will_be_done = dealer_ai(player, dealer)
+
         else:
             break
 
